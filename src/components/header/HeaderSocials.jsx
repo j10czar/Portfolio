7ed188270af2b79afc4e5fb6058359227ae37e33
Jason Tenczar <jasontenczar@gmail.com>
2022-06-28 21:24:30 -0400

    stopped eslint errors

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://github.com/j10czar" target="_blank"><FaGithub/></a>
+        <a href="https://github.com/j10czar" target="_blank"><FaGithub/></a>{/* eslint-disable-line */}
     </div>
   )
 }

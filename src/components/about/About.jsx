1e89dd756dbb2376022d1b7d51fc196e1b5214e7
j10 <jasontenczar@gmail.com>
2024-09-11 20:30:11 -0400

    changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         </div>
 
         <div className='about__content'>
-          <p>Hello, I’m Jason, a sophmore at the University of Florida studying computer science. I have a deep passion for AI and the innovative ways technology can transform businesses. As a full-stack developer, I’m proficient in both web design and backend technologies such as AWS, which enhances my ability to create impactful digital solutions. I previously was a Software Engineering Intern at Integris Group, where I gained valuable hands-on experience in both frontend and backend AI development. As I continue my education, I’m particularly excited about exploring artificial intelligence and its applications in real-world scenarios. This interest is guiding my academic and professional journey as I seek to blend technology and business in meaningful ways.</p>
+          <p>Hello, I’m Jason, a student at the University of Florida studying computer science. I have a deep passion for AI and the innovative ways technology can transform businesses. As a full-stack developer, I’m proficient in both web design and backend technologies such as AWS, which enhances my ability to create impactful digital solutions. I previously was a Software Engineering Intern at Integris Group, where I gained valuable hands-on experience in both frontend and backend AI development. As I continue my education, I’m particularly excited about exploring artificial intelligence and its applications in real-world scenarios. This interest is guiding my academic and professional journey as I seek to blend technology and business in meaningful ways.</p>
         </div>
       </div>
  
